d70570951952c72ccc188c43633e8f5438f8f2ed
ctyFL <502651020@qq.com>
2020-05-17 19:09:03 +0800

    工具类测试

--- helper/utils/src/test/TestMain.java
+++ helper/utils/src/test/TestMain.java
@@ -1,5 +1,8 @@
 package test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import utils.DateUtil_FL;
 import utils.ExcelUtil_FL;
 import utils.FileUtil_FL;
@@ -10,34 +13,44 @@ import utils.wxUtils.AccessToken_FL;
 public class TestMain {
 
 	public static void main(String[] args) {
-		String date1 = "2019.8";
-		String date2 = "2020.1";
-		String date3 = "2015.11";
-		
-		String str1 = DateUtil_FL.getTimeIntervalByTwoDates(date1, date2, DateUtil_FL.ONLY_MONTH);
-		String str2 = DateUtil_FL.getTimeIntervalByTwoDates(date2, date3, DateUtil_FL.YEAR_AND_MONTH);
-		
-		System.out.println(str1);
-		System.out.println(str2);
-		
-		String ss = "sdfjlg?#$%^&sdfsd()&%^~!@#$,./.=-0";
-		System.out.println(StringUtil_FL.filterSpecialCharacterFromString(ss));
-		
-		String string = "dfsag《，。？！’‘“”《》￥";
-		System.out.println(StringUtil_FL.convertPunctuationToEnglishStatus(string));
-		
-		System.out.println(StringUtil_FL.getNowDateCurrentTimeStr());
-		
-		String servletName = "http://192.168.10.153:9100/AriesFL/ReceiveFileServlet";
-		FileUtil_FL.uploadToServer("D:/31.jpg", servletName);
-		
-		HttpUtil_FL.HttpGetThread(null, servletName);
-		HttpUtil_FL.HttpPostThread(null, servletName);
-		
-		ExcelUtil_FL.readXlsxExcelByPOI("D:/22.xlsx");
+//		String date1 = "2019.8";
+//		String date2 = "2020.1";
+//		String date3 = "2015.11";
+//		
+//		String str1 = DateUtil_FL.getTimeIntervalByTwoDates(date1, date2, DateUtil_FL.ONLY_MONTH);
+//		String str2 = DateUtil_FL.getTimeIntervalByTwoDates(date2, date3, DateUtil_FL.YEAR_AND_MONTH);
+//		
+//		System.out.println(str1);
+//		System.out.println(str2);
+//		
+//		String ss = "sdfjlg?#$%^&sdfsd()&%^~!@#$,./.=-0";
+//		System.out.println(StringUtil_FL.filterSpecialCharacterFromString(ss));
+//		
+//		String string = "dfsag《，。？！’‘“”《》￥";
+//		System.out.println(StringUtil_FL.convertPunctuationToEnglishStatus(string));
+//		
+//		System.out.println(StringUtil_FL.getNowDateCurrentTimeStr());
+//		
+//		String servletName = "http://192.168.10.153:9100/AriesFL/ReceiveFileServlet";
+//		FileUtil_FL.uploadToServer("D:/31.jpg", servletName);
+//		
+//		HttpUtil_FL.HttpGetThread(null, servletName);
+//		HttpUtil_FL.HttpPostThread(null, servletName);
+//		
+//		ExcelUtil_FL.readXlsxExcelByPOI("D:/22.xlsx");
 		
 		AccessToken_FL accessToken_FL = AccessToken_FL.getInstance();
-		System.out.println(accessToken_FL.getMiniAppAccessToken());
+		String access_token = accessToken_FL.getMiniAppAccessToken();
+		System.out.println();
+		
+		String josnStr = "{'content':'hello'}";
+		String url = "https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=" + access_token;
+		Map<String, String> paramsMap = new HashMap<String, String>();
+		paramsMap.put("touser", "oS2PU5LK8MoZ09gvhc8iTSoEnm_U");
+		paramsMap.put("msgtype", "text");
+		paramsMap.put("text", josnStr);
+		String result = HttpUtil_FL.HttpPost(paramsMap, url);
+		System.out.println(result);
 	}
 
 }
